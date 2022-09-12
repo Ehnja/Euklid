dec73cb19b279932249b7fbc89a09b64bfcc26bd
Hilmer Oudijk <hilmeroudijk@gmail.com>
2022-09-12 16:51:34 +0200

    First commit

---
+++ Euklid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Euklid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GcdR(152, 3384));
+            Console.WriteLine(GcdT(33384, 152));
+        }
+        static int GcdR(int m, int n)
+        {
+            if (m % n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return GcdR(n, m % n);
+            }
+        }
+        static int GcdT(int m, int n)
+        {
+            int tmp;
+            while (m%n > 0)
+            {
+                tmp = n;
+                n = m % n;
+                m = tmp;
+            }
+            return n;
+        }
+    }
+}
